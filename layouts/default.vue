199f83ec1f8b31f5d97792d8bb3a57273372fcbb
Michaela Havlíková <Michelle.h@seznam.cz>
2025-05-14 10:00:03 +0200

    add default layout template with navbar and footer

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-h-screen flex flex-col bg-white text-primary">
+    <AppNavbar />
+    <main class="flex-grow">
+      <slot />
+    </main>
+    <AppFooter />
+  </div>
+</template>
