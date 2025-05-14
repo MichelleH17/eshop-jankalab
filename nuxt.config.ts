22fbd2f9c658ff00e28036567b731b5ce53a5fda
Michaela Havlíková <Michelle.h@seznam.cz>
2025-05-14 09:57:55 +0200

    add configuration for image handling, custom icon collections, and Tailwind CSS theme extensions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,4 +9,41 @@ export default defineNuxtConfig({
     "@nuxt/eslint"
   ],
   devtools: { enabled: true },
+  image: {
+    dir: "public",
+  },
+  css: ["@/assets/css/main.css"],
+  compatibilityDate: "2024-11-01",
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+  icon: {
+    customCollections: [
+      {
+        prefix: "icon",
+        dir: "./assets/icons",
+      },
+    ],
+  },
+  tailwindcss: {
+    config: {
+      theme: {
+        extend: {
+          colors: {
+            primary: "#0a0a0f",
+            gray_light: "#f5f5f5",
+            gray_dark: "#9d9d9d",
+            gray: "#f9f9f9",
+            accent: "#e8e1d4",
+          },
+          fontFamily: {
+            youth: ["Youth", "sans-serif"],
+            montserrat: ["Montserrat", "sans-serif"],
+          },
+        },
+      },
+    },
+  },
 });
