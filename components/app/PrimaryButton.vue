e4d18927ea96c802e5fdfe7c3e1a63bcf3872d41
Michaela Havlíková <Michelle.h@seznam.cz>
2025-05-14 10:01:50 +0200

    add PrimaryButton component

---
+++ components/app/PrimaryButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <button
+      class="bg-black hover:bg-white text-white hover:text-black border border-white hover:border-black px-4 py-3 rounded-xl text-lg w-auto mx-auto md:mx-0 md:self-start"
+    >
+      <slot />
+    </button>
+  </div>
+</template>
