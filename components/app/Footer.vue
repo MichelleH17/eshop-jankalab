4306539bd2cd1681220efb8859409cc72b2966f1
Michaela Havlíková <Michelle.h@seznam.cz>
2025-05-14 10:00:34 +0200

    add Footer component

---
+++ components/app/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer
+    class="text-lg mt-4 px-[47px] flex flex-col md:flex-row text-center md:justify-between bg-gray_light py-2 whitespace-nowrap"
+  >
+    <p>© 2025 Jan Kaláb. Všechna práva vyhrazena.</p>
+    <span class="inline-flex items-center justify-center gap-1">
+      <p>Created by</p>
+      <Icon
+        name="icon:monster-media"
+        size="30px"
+      /> Monster Media, s. r. o.
+    </span>
+  </footer>
+</template>
